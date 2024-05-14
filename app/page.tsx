48b77a13e02906ccf247cd42602e2948074db923
Efetomeh <167780636+senpaiefetomeh@users.noreply.github.com>
2024-05-14 19:30:00 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        hi, EFETOMEH here
+        hi ツ, EFETOMEH here
       </h1>
       <p className="mb-4">
         {`passionate about the intersection of artificial intelligence and neuroscience.
