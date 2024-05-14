3714fd566162f55e354e94003ee65b90da528e2a
Efetomeh <167780636+senpaiefetomeh@users.noreply.github.com>
2024-05-14 18:51:18 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
       <p className="mb-4">
         {`Passionate about the intersection of artificial intelligence and neuroscience.
         `}
-        PS: Low-key <a href="https://www.coursera.org/account/accomplishments/specialization/JAC75YNQH5TX">DeepLearning.AI</a> and <a href="https://www.coursera.org/account/accomplishments/professional-cert/FF2K7G6ESKW9" target="_blank">TensorFlow</a> Certified.
+        PS: Low-key <a href="https://www.coursera.org/account/accomplishments/specialization/JAC75YNQH5TX"><u>DeepLearning.AI</u></a> and <a href="https://www.coursera.org/account/accomplishments/professional-cert/FF2K7G6ESKW9" target="_blank"><u>TensorFlow</u></a> Certified.
 
       </p>
       <div className="my-8">
