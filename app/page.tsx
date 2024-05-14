d933e4d6f6adc904049f9bfe76d48d4f06fbca5d
Efetomeh <167780636+senpaiefetomeh@users.noreply.github.com>
2024-05-14 18:57:02 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,15 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        hi, EFE here
+        hi, EFETOMEH here
       </h1>
       <p className="mb-4">
         {`Passionate about the intersection of artificial intelligence and neuroscience.
         `}
-        PS: Low-key <a href="https://www.coursera.org/account/accomplishments/specialization/JAC75YNQH5TX"><u>DeepLearning.AI</u></a> and <a href="https://www.coursera.org/account/accomplishments/professional-cert/FF2K7G6ESKW9" target="_blank"><u>TensorFlow</u></a> Certified.
-
+      </p>
+      <p>
+        PS: Low-key <a href="https://www.coursera.org/account/accomplishments/specialization/JAC75YNQH5TX"><u>DeepLearning.AI</u></a>
+        and <a href="https://www.coursera.org/account/accomplishments/professional-cert/FF2K7G6ESKW9" target="_blank"><u>TensorFlow</u></a> Certified.
       </p>
       <div className="my-8">
         <BlogPosts />
