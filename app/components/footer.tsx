bc90778c30baec3166eb6beb6e38722b38ecad99
Efetomeh <167780636+iamefetomeh@users.noreply.github.com>
2024-05-16 14:08:48 +0100

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -68,6 +68,17 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
+            href="https://x"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">[matrix]</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
             href="https://www.billwinston.org/"
           >
             <ArrowIcon />
